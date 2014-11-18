f8f27213cbb08a6e75491ca5ccad2465e41c3ca5
Mamoudou Camara <amos2013.camara02@gmail.com>
2014-11-18 12:58:50 +0100

    Changes commit

---
+++ Mvc-Git-Demo/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Git_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This is an Index side!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mvc-Git-Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc_Git_Demo.MvcApplication" Language="C#" %>
